0f423110f1c9c2c1283515371c5623c604f05d48
Reed Goodwin <113049336+Soup-Mann@users.noreply.github.com>
2022-10-19 10:37:29 -0700

    FirstPush

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿//Reed Goodwin
+//10-19-2022
+//Mini CHallenge #4
+//THis is a console that will tell you which number is
+//Greater, less than, or equal too
+// Its also very polite :) 
+
+Console.Clear();
+
+string stop = "";
+while(stop != "no"){
+Console.WriteLine("");
+
+Console.WriteLine("GREATER THAN OR LESS THAN");
+Console.WriteLine("");
+
+Console.WriteLine("Please Enter a number!");
+
+string thisnumber = Console.ReadLine();
+Console.WriteLine("");
+Console.WriteLine("Please Enter a second number!");
+
+string thatnumber = Console.ReadLine();
+
+int thisnumbertwo = Convert.ToInt32(thisnumber);
+int thatnumbertwo = Convert.ToInt32(thatnumber);
+
+
+if( thisnumbertwo > thatnumbertwo ){
+    Console.WriteLine("");
+    Console.WriteLine(thisnumber + " Is greater than " + thatnumber + "!" );
+} else if( thisnumber == thatnumber ){
+    Console.WriteLine("");
+Console.WriteLine( thisnumber + " Is equal too " + thatnumber + "!" );
+}else{
+    Console.WriteLine("");
+    Console.WriteLine(thisnumber + " Is less than " + thatnumber + "!" );
+}
+Console.WriteLine("");
+Console.WriteLine("Wow! Your so cool! Keep it up!");
+Console.WriteLine("");
+Console.WriteLine("Thank you for using my program :)");
+Console.WriteLine("");
+Console.WriteLine("Would you like to play again?");
+stop = Console.ReadLine();
+}
